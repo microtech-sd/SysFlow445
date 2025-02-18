5f1c5014a2654af235a1a85613a055216d483799
Mohmmed Jamal <152101521+microtech-sd@users.noreply.github.com>
2025-02-18 04:26:34 +0400

    Update exergy.py

--- utils/exergy.py
+++ utils/exergy.py
@@ -1,1 +1,7 @@
+def calculate_exergy(enthalpy, entropy, ambient_temp):
+    return enthalpy - ambient_temp * entropy
+
+def exergy_efficiency(enthalpy, exergy):
+    total = enthalpy + exergy
+    return (enthalpy / total) * 100, (exergy / total) * 100
 

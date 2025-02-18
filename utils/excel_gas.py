afef28db3a371c2bb43d561fff3fcbf9bf71294e
Mohmmed Jamal <152101521+microtech-sd@users.noreply.github.com>
2025-02-18 04:27:02 +0400

    Update excel_gas.py

--- utils/excel_gas.py
+++ utils/excel_gas.py
@@ -1,1 +1,7 @@
+import pandas as pd
+
+def read_excel_data(uploaded_file):
+    # Read the Excel file into a DataFrame
+    data = pd.read_excel(uploaded_file, sheet_name="Sheet1")
+    return data
 

3d08dd6d56c610022c559a4824fe519abe0eb682
Mohmmed Jamal <152101521+microtech-sd@users.noreply.github.com>
2025-02-18 04:24:19 +0400

    Update real_fluid.py

--- models/real_fluid.py
+++ models/real_fluid.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def calculate_real_fluid(temperature, pressure, data):
+    # Assuming data is a pandas DataFrame loaded from Excel
+    fluid_data = data[(data['Temperature'] == temperature) & (data['Pressure'] == pressure)]
+    if fluid_data.empty:
+        return None, None, None
+    else:
+        return fluid_data['Enthalpy'].values[0], fluid_data['Entropy'].values[0], fluid_data['Specific Volume'].values[0]
